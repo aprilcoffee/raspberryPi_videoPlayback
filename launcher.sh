b1fe3f475e38129c8efb8dcaa5cc1efb4b0e03b0
aprilcoffee <8765498@gmail.com>
2020-11-13 17:25:40 +0100

    update

--- launcher.sh
+++ launcher.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # launcher.sh
 # navigate to home directory, then to this directory, then execute python script, then back home
-
+sleep 5
 cd /
 cd /home/pi/Desktop/raspberryPi_videoPlayback/
-sudo omxplayer --no-osd --loop -b --aspect-mode fill video.mp4 
+omxplayer --no-osd --loop -b --aspect-mode fill test_pattern_1080p.mp4 
 cd /
